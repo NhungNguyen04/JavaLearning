59d222c2007ceb21ef7c3663ae65c1af1fd8576b
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-02-22 10:58:05 +0700

    classes

--- FirstJavaProject/src/Main.java
+++ FirstJavaProject/src/Main.java
@@ -2,19 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Primitive types
-        int a = 5;
-        char b = '1';
-        long c = 49999;
-        double d = 3.2;
+//        // Primitive types
+//        int a = 5;
+//        char b = '1';
+//        long c = 49999;
+//        double d = 3.2;
+//
+//        // Non-primitive types
+//        String name = "Nhung Hong";
+//        System.out.println(name.toUpperCase());
+//        addExclamationPoint(name);
 
-        // Non-primitive types
-        String name = "Nhung Hong";
-        System.out.println(name.toUpperCase());
-        addExclamationPoint(name);
+        Animal a = new Animal();
+        String sentence = a.iAmDog();
+        System.out.println(sentence);
     }
 
-    public static void addExclamationPoint(String s){
-        System.out.println(s + "!");
-    }
+//    public static void addExclamationPoint(String s){
+//        System.out.println(s + "!");
+//    }
 }
