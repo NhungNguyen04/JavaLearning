dcb8325b5209e4392efd606301b0b1cd18c69aa6
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-02-29 14:43:41 +0700

    GUI

--- eclipse-workspace/GUIIntro/src/Main.java
+++ eclipse-workspace/GUIIntro/src/Main.java
@@ -1,4 +1,16 @@
+import javax.swing.JOptionPane;
 
 public class Main {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("Write your name");
+		JOptionPane.showMessageDialog(null, "Hello " + name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("How old are you? "));
+		JOptionPane.showMessageDialog(null, "You are " + age + " years old");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("How tall are you? "));
+		JOptionPane.showMessageDialog(null, "You are " + height + " cm tall");
+	}
 
 }
+ 
