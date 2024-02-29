387cea1a9abcac0755ea80ab4b629a8aed04aa55
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-02-29 15:11:38 +0700

    MathClass

---
+++ eclipse-workspace/MathClass/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		double x = 9.439;
+		double y = 9.230;
+		
+		System.out.println(Math.max(x, y));
+		
+		double a, b, c;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter side a: ");
+		a = scanner.nextDouble();
+		System.out.println("Enter side b: ");
+		b = scanner.nextDouble();
+		
+		System.out.println(Math.sqrt(a*a+b*b));
+		
+		scanner.close();
+	}
+}
