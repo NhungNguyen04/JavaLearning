9b6d21c88005c1dcf8d2d47e5f1b78781296a6d5
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-03 09:56:19 +0700

    Wrapper class

---
+++ eclipse-workspace/WrapperClass/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+//	wrapper class = provides a wayto use primitive datatypes as reference datatypes
+//					reference datatypes contain useful methods
+//					can be used with collections (ex.ArrayList)
+//					
+//	primitive		wrapper
+//	-----------------------
+//	boolean			Boolean
+//	char			Character
+//	int				Integer
+//	double			Double
+//	
+//	autoboxing = the automatic convversion that Java compiler makes between the primitive types and wrapper classes
+//	unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
+	
+	Boolean a = true;
+	Character c = 'n';
+	Integer i = 15;
+	Double d = 3.13;
+	String s = "hi";
+}
