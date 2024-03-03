78ac50a9e917c6ee1e58b4b7ce150b4f256980fd
NGUYEN THI HONG NHUNG <143203553+NhungNguyen04@users.noreply.github.com>
2024-03-03 09:46:06 +0700

    String

---
+++ eclipse-workspace/String/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	public static void main(String[] ars) {
+		String name = "   Nhung";
+		
+//		boolean result = name.equalsIgnoreCase("nhung");
+//		int result = name.length();
+//		char result = name.charAt(4);
+//		int result = name.indexOf("n");
+//		boolean result = name.isEmpty();
+//		String result = name.toLowerCase();
+//		String result = name.toUpperCase();
+//		String result = name.trim();
+		String result = name.replace('g', 'x');
+		
+		System.out.println(result);
+	}
+}
